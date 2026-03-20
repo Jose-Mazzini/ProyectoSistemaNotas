ff08d91e2560c71865acc83fc2c4b4e53237fffd
JavoMCL <hahusussu282828@gmail.com>
2026-03-20 16:15:10 -0300

    Se agrego manejo de excepciones con GlobalExceptionHandler y ResourceNotFoundException

---
+++ src/main/java/Facyt/Proyecto_Sistema_2026/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package Facyt.Proyecto_Sistema_2026.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+        public ResponseEntity<Map<String, Object>> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request){
+            Map<String, Object> response = new HashMap<>();
+            response.put("timestamp", LocalDateTime.now());
+            response.put("status", 404);
+            response.put("error", "Not Found");
+            response.put("message", ex.getMessage());
+            response.put("path", request.getDescription(false).replace("uri=", ""));
+
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGlobalException(Exception ex, WebRequest request){
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", 500);
+        response.put("error", "Internal Server Error");
+        response.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
